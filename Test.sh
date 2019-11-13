4c929f9d8bf8f5cbafe105f3dfdb6fc3c79d3e1e
allenroberts1 <50081814+allenroberts1@users.noreply.github.com>
2019-11-13 11:05:41 -0500

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -1,5 +1,5 @@
 javac CSC499Part1.java
 java CSC499Part1 "Sort Me.txt" "outputAsc" "a"
-diff outputAsc.txt SortedText.txt
+diff outputAsc.txt SortedAsc.txt
 java CSC499Part1 "Sort Me.txt" "outputDesc" "d"
 diff outputDesc.txt SortedDesc.txt
